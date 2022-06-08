e449f034c2973fcfc318998b9a09bb53ba87bad0
ffouquet42 <ffouquet42@gmail.com>
2022-06-08 10:03:33 +0200

    add prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,16 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 14:53:20 by fllanet           #+#    #+#             */
-/*   Updated: 2022/06/05 14:56:16 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/06/08 09:35:53 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <stdio.h> // Delete
+
+int	main(void); // Delete
+
+char	*get_next_line(int fd);
 
 #endif
