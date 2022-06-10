8dab15a8cb18860317433d0b0b309646cb5b9f65
ffouquet42 <ffouquet42@gmail.com>
2022-06-10 08:36:40 +0200

    call get_next_line function

--- main.c
+++ main.c
@@ -6,14 +6,18 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 09:29:01 by fllanet           #+#    #+#             */
-/*   Updated: 2022/06/08 09:36:58 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/06/10 08:32:20 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 int	main(void)
 {
-	
+	int fd = 1;
+	write(1, "start\n", 6); // debug
+	printf("%s", get_next_line(fd));
+	write(1, "end\n", 4); // debug
 	return (0);
 }
